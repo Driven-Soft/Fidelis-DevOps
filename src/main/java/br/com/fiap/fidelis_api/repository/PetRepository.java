73079d6bf19be105da355aeac6fa22aa991737f6
Max Hayashi Batista <rm563717@fiap.com.br>
2026-05-21 15:34:54 -0300

    fix: removida api java de testes

--- src/main/java/br/com/fiap/fidelis_api/repository/PetRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.fidelis_api.repository;
-
-import br.com.fiap.fidelis_api.entity.Pet;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
