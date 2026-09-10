4a55e3948267da0af50a1fe52510759aa5ee6c5f
Lucas <silvalucas71743@gmail.com>
2026-09-10 15:18:28 -0300

    feat: configura CORS e middleware JWT

--- Fidelis.Api/Program.cs
+++ Fidelis.Api/Program.cs
@@ -1,20 +1,74 @@
+using System.Text;
+
 using Fidelis.Api.Exceptions;
 using Fidelis.Api.Extensions;
 
 using Fidelis.Infrastructure.Persistence;
 using Fidelis.Infrastructure.Persistence.Seed;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Mobile", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:8081")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
+
 builder.Services.AddSwagger();
+
 builder.Services.AddServices();
 builder.Services.AddRepositories();
 builder.Services.AddDbContext(builder.Configuration);
+
+var jwtKey = builder.Configuration["JWT_KEY"];
+
+if (string.IsNullOrWhiteSpace(jwtKey))
+{
+    throw new InvalidOperationException(
+        "A variável de ambiente JWT_KEY não foi configurada."
+    );
+}
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtKey)
+            ),
+
+            ValidateIssuer = true,
+            ValidIssuer = "Fidelis.Api",
+
+            ValidateAudience = true,
+            ValidAudience = "Fidelis.Mobile",
+
+            ValidateLifetime = true,
+
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -22,7 +76,12 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors("Mobile");
+
 app.UseExceptionHandler();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
